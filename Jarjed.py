6603ab15e7a20a4daaf7bddf0bb9a9b119de2412
opilane <opilane@HP-CZC6297BB1>
2021-12-03 10:01:13 +0200

    asdasdasd

--- Jarjed.py
+++ Jarjed.py
@@ -87,39 +87,85 @@
 #print(spisok)
 
 #4 задание
-from random import *
-spisok=[]
-N=randint(2,20)
-for i in range(N):
-	spisok.append(randint(-50,50))
-print(spisok)
+#from random import *
+#spisok=[]
+#N=randint(2,20)
+#for i in range(N):
+#	spisok.append(randint(-50,50))
+#print(spisok)
+#while 1:
+#	try:
+#		k=int(input("С какой позиции начаинать - "))
+#		if k<=N//2:
+#			break
+#	except ValueError:
+#		print("Значение должно быть меньше, чем N//2")
+#k-=1
+#for i in range(k,-1,-1):
+#	print(spisok[i-1],end=" <-> ")
+#	print(spisok[N-i-1]) #print(spisok[-1*i])
+#	a=spisok.pop(i)
+#	spisok.insert(N-i-1-1,a)
+#	a=spisok.pop(N-i-1)
+#	spisok.insert(i,a)
+#print(spisok)
+#max=-50
+#for element in spisok:
+#	if element>max: max=element
+#new_max=max/N #/len(spisok)
+#ind=spisok.index(max)
+#spisok.remove(max)
+#spisok.insert(ind,new_max)
+#print(spisok)
+#spisok2=[]
+#for e in spisok:
+#	spisok2.append(abs(e))
+#print(spisok2.sort())
+#print(spisok2.sort(reverse=True))
+
+
+# 5 задание
+#l=['крот', 'белка', 'выхухоль']
+#nl=[]
+#max=0
+#for e in l:
+#	x=len(e)
+#	if x>max:
+#		max=x
+#for e in l:
+#	nl.append(e.ljust(max,"_"))
+#print(nl)
+
+#from random import*
+#s1=['крот', 'белка', 'выхухоль']
+#s2=['a', 'aa', 'aaa', 'aaaa', 'aaaaa']
+#s3=['qweasdqweas', 'q', 'rteww', 'ewqqqqq']
+#ss=[s1,s2,s3]
+#N=0
+#while N<len(ss):
+#	print(ss[N])
+#	max=0
+#	sN_=[]
+#	for s in ss[N]:
+#		pikkus=len(s)
+#		if pikkus>max: max=pikkus
+#	for s in ss[N]:
+#		sN_.append(s.ljust(max,"_"))
+#	print(sN_)
+#	N+=1
+
+# 6 задание
 while 1:
 	try:
-		k=int(input("С какой позиции начаинать - "))
-		if k<=N//2:
-			break
+		nimi=input("Ввеите своё имя - ")
+		break
 	except ValueError:
-		print("Значение должно быть меньше, чем N//2")
-k-=1
-for i in range(k,-1,-1):
-	print(spisok[i-1],end=" <-> ")
-	print(spisok[N-i-1]) #print(spisok[-1*i])
-	a=spisok.pop(i)
-	spisok.insert(N-i-1-1,a)
-	a=spisok.pop(N-i-1)
-	spisok.insert(i,a)
-print(spisok)
-max=-50
-for element in spisok:
-	if element>max: max=element
-new_max=max/N #/len(spisok)
-ind=spisok.index(max)
-spisok.remove(max)
-spisok.insert(ind,new_max)
-print(spisok)
-spisok2=[]
-for e in spisok:
-	spisok2.append(abs(e))
-print(spisok2.sort())
-print(spisok2.sort(reverse=True))
-# 5 задание
+		print("Имена состоят только из букв!")
+print("Добро пожаловать,",nimi.title())
+
+
+
+
+#uksikud=[x for x in abc if abc.count(x)>=1]
+#uksikud=list(set(uksikud))
+#print(uksikud)
